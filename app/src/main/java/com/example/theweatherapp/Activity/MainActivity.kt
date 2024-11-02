76bf6e38dea0715428ad2acc044752774d8beb91
nhat nguyen <nhat09166@gmail.com>
2024-11-02 23:37:57 +0700

    second commit

--- app/src/main/java/com/example/theweatherapp/Activity/MainActivity.kt
+++ app/src/main/java/com/example/theweatherapp/Activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.theweatherapp
+package com.example.theweatherapp.Activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.theweatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
